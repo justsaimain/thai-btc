9a1a998dca12dec8453ca1d0c30ab6a2a6060783
Sai Main <saimain.venuslab@gmail.com>
2022-06-05 09:59:13 +0630

    fix login

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");i
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const cron = require("node-cron");
